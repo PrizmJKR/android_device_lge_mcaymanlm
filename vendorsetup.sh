7585ec87698604511f58e48abf7ae1ddb61aa3ec
PrizmJKR <75905361+PrizmJKR@users.noreply.github.com>
2023-01-09 21:15:41 -0800

    Comment out uneeded lines

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_mcaymanlm-user
-add_lunch_combo omni_mcaymanlm-userdebug
+#add_lunch_combo omni_mcaymanlm-user
+#add_lunch_combo omni_mcaymanlm-userdebug
 add_lunch_combo omni_mcaymanlm-eng
